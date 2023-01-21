01b3e30ffda9ba2bf4f8a9457f5137732a757ec7
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-01-21 12:48:36 -0300

    :sparkles: ajustes no recebimento da API

--- index.js
+++ index.js
@@ -12,19 +12,19 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
-app.use("api/v1/post", postRoutes);
-app.use("api/v1/dalle", dalleRoutes);
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", async (req, res) => {
-	res.send("Hello from DALL-E!");
+	res.status(200).json({
+		message: "Hello from DALL.E!",
+	});
 });
 
 const startServer = async () => {
 	try {
 		connectDB(process.env.MONGODB_URL);
-		app.listen(8000, () =>
-			console.log("Server has started on port http://localhost:8000")
-		);
+		app.listen(8080, () => console.log("Server started on port 8080"));
 	} catch (error) {
 		console.log(error);
 	}
--- routes/dalleRoutes.js
+++ routes/dalleRoutes.js
@@ -13,7 +13,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 router.route("/").get((req, res) => {
-	res.send("Hello from DALL-E!");
+	res.status(200).json({ message: "Hello from DALL-E!" });
 });
 
 router.route("/").post(async (req, res) => {
@@ -28,11 +28,12 @@ router.route("/").post(async (req, res) => {
 		});
 
 		const image = aiResponse.data.data[0].b64_json;
-
 		res.status(200).json({ photo: image });
 	} catch (error) {
-		console.log(error);
-		res.status(500).send(error?.response.data.error.message);
+		console.error(error);
+		res
+			.status(500)
+			.send(error?.response.data.error.message || "Something went wrong");
 	}
 });
 
